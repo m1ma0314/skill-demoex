33e855d2a90c9d7082e7b76cc2049e35f3e59267
m1ma0314 <m1ma@ucsd.edu>
2022-02-03 23:32:48 -0800

    error fix

--- SkillDemoexTest.java
+++ SkillDemoexTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoexTest {
     @Test
     public void multiplication() {
-        assertEquals(5, SkillDemoex.multiply(2, 3));
+        assertEquals(6, SkillDemoex.multiply(2, 3));
     }
 }
